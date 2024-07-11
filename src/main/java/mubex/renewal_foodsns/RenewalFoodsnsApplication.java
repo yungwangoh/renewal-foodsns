5d05ddbf1e3502f9f7f587a36d508016c9578361
swager253 <swager253@naver.com>
2024-07-11 18:33:11 +0900

    project start

---
+++ src/main/java/mubex/renewal_foodsns/RenewalFoodsnsApplication.java
@@ -0,0 +1,13 @@
+package mubex.renewal_foodsns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenewalFoodsnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenewalFoodsnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mubex/renewal_foodsns/RenewalFoodsnsApplicationTests.java
@@ -0,0 +1,13 @@
+package mubex.renewal_foodsns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenewalFoodsnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
