474eb543bd218d9287883e8f561589922476a98a
swager253 <swager253@naver.com>
2024-07-12 00:02:52 +0900

    test: 멤버 랭크 업데이트 테스트

---
+++ src/test/java/mubex/renewal_foodsns/domain/member/entity/MemberTest.java
@@ -0,0 +1,52 @@
+package mubex.renewal_foodsns.domain.member.entity;
+
+import static org.assertj.core.api.Assertions.*;
+
+import mubex.renewal_foodsns.domain.member.MemberRank;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+class MemberTest {
+
+    @ParameterizedTest
+    @ValueSource(longs = {150, 250, 250})
+    void 유저의_좋아요의_개수에_따라_등급이_변한다(long heart) {
+
+        Member member = Member.create(
+                "name",
+                "pwd",
+                "email",
+                heart,
+                1,
+                false,
+                MemberRank.NORMAL,
+                false);
+
+        member.updateMemberRank(MemberRank.convert(heart));
+
+        assertThat(member.getMemberRank()).isEqualTo(MemberRank.convert(heart));
+    }
+
+    @Test
+    void 유저의_좋아요_개수가_올바른_입력이_아닐_경우() {
+
+        long invalidHeart = -1;
+
+        Member member = Member.create(
+                "name",
+                "pwd",
+                "email",
+                0,
+                1,
+                false,
+                MemberRank.NORMAL,
+                false);
+
+        assertThatThrownBy(() -> member.updateMemberRank(MemberRank.convert(invalidHeart)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
