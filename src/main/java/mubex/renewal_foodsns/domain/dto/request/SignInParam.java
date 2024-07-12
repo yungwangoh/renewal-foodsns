6f9b04b982c9b47e8145fee5b1b49207f9b02f3e
swager253 <swager253@naver.com>
2024-07-12 22:46:22 +0900

    feat: 로그인 param dto 추가

---
+++ src/main/java/mubex/renewal_foodsns/domain/dto/request/SignInParam.java
@@ -0,0 +1,18 @@
+package mubex.renewal_foodsns.domain.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record SignInParam(
+        @Email
+        @NotBlank
+        String email,
+
+        @NotBlank
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$",
+                message = "비밀번호는 8~20자의 영문 대/소문자, 숫자, 특수문자를 포함해야 합니다."
+        )
+        String password) {
+}
