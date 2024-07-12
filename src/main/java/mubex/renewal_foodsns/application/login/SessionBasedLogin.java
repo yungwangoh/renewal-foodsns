569df55a54c4d952bc4af66ad6bbf44df6d15cb4
swager253 <swager253@naver.com>
2024-07-12 22:46:41 +0900

    feat: 세션 방식 로그인 추가

---
+++ src/main/java/mubex/renewal_foodsns/application/login/SessionBasedLogin.java
@@ -0,0 +1,33 @@
+package mubex.renewal_foodsns.application.login;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import mubex.renewal_foodsns.application.mapper.MemberMapper;
+import mubex.renewal_foodsns.domain.dto.response.MemberResponse;
+import mubex.renewal_foodsns.domain.entity.Member;
+import mubex.renewal_foodsns.domain.repository.MemberRepository;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Primary
+@Component
+@RequiredArgsConstructor
+public class SessionBasedLogin implements LoginHandler {
+
+    private final HttpSession httpSession;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public MemberResponse signIn(String email, String password) {
+        Member member = memberRepository.findByEmailAndPassword(email, password);
+
+        httpSession.setAttribute(member.getNickName(), member);
+
+        return MemberMapper.INSTANCE.toResponse(member);
+    }
+
+    @Override
+    public void signOut(String nickName) {
+        httpSession.removeAttribute(nickName);
+    }
+}
