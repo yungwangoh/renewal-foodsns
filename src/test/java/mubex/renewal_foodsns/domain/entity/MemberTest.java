628bbd65d804d35efe2866e892897f7e99ec5f84
swager253 <swager253@naver.com>
2024-07-12 22:50:18 +0900

    test: 유저 도메인 테스트

--- src/test/java/mubex/renewal_foodsns/domain/entity/MemberTest.java
+++ src/test/java/mubex/renewal_foodsns/domain/entity/MemberTest.java
@@ -2,7 +2,6 @@ package mubex.renewal_foodsns.domain.entity;
 
 import static org.assertj.core.api.Assertions.*;
 
-import mubex.renewal_foodsns.domain.entity.Member;
 import mubex.renewal_foodsns.domain.type.MemberRank;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
@@ -16,7 +15,7 @@ class MemberTest {
     @ParameterizedTest
     @ValueSource(longs = {150, 250, 250})
     void 유저의_좋아요의_개수에_따라_등급이_변한다(long heart) {
-
+        // given
         Member member = Member.create(
                 "name",
                 "pwd",
@@ -27,8 +26,10 @@ class MemberTest {
                 MemberRank.NORMAL,
                 false);
 
+        // when
         member.updateMemberRank(MemberRank.convert(heart));
 
+        // then
         assertThat(member.getMemberRank()).isEqualTo(MemberRank.convert(heart));
     }
 
