aa8f66f596a0053e06141b5ab2d092be05395557
swager253 <swager253@naver.com>
2024-07-12 22:47:26 +0900

    feat: 로그인, 아웃 기능 OAuth, JWT 도입을 위한 추상화

---
+++ src/main/java/mubex/renewal_foodsns/application/login/LoginHandler.java
@@ -0,0 +1,9 @@
+package mubex.renewal_foodsns.application.login;
+
+import mubex.renewal_foodsns.domain.dto.response.MemberResponse;
+
+public interface LoginHandler {
+
+    MemberResponse signIn(String email, String password);
+    void signOut(String nickName);
+}
