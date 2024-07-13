c83455b3c506f62c96d0e3b6e0b3fbdb6f56045f
swager253 <swager253@naver.com>
2024-07-13 21:41:02 +0900

    feat: 유저 DTO 상세 정보 더 추가

--- src/main/java/mubex/renewal_foodsns/domain/dto/response/MemberResponse.java
+++ src/main/java/mubex/renewal_foodsns/domain/dto/response/MemberResponse.java
@@ -1,10 +1,14 @@
 package mubex.renewal_foodsns.domain.dto.response;
 
+import lombok.Builder;
 import mubex.renewal_foodsns.domain.type.MemberRank;
 
+@Builder
 public record MemberResponse(
+        int profileId,
         String nickName,
         long heart,
-        MemberRank memberRank
-) {
+        MemberRank memberRank,
+        int report,
+        boolean inBlackList) {
 }
