eedd7e44368d9e7e1ca41aa14c513c12c6a80559
swager253 <swager253@naver.com>
2024-07-13 21:39:52 +0900

    feat: 유저 삭제 API

--- src/main/java/mubex/renewal_foodsns/presentation/MemberApi.java
+++ src/main/java/mubex/renewal_foodsns/presentation/MemberApi.java
@@ -1,29 +1,32 @@
 package mubex.renewal_foodsns.presentation;
 
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mubex.renewal_foodsns.application.MemberService;
-import mubex.renewal_foodsns.domain.dto.request.SignInParam;
-import mubex.renewal_foodsns.domain.dto.request.SignOutParam;
-import mubex.renewal_foodsns.domain.dto.request.SignUpParam;
-import mubex.renewal_foodsns.domain.dto.request.UpdateNickNameParam;
-import mubex.renewal_foodsns.domain.dto.request.UpdatePasswordParam;
+import mubex.renewal_foodsns.domain.dto.request.sign.SignInParam;
+import mubex.renewal_foodsns.domain.dto.request.sign.SignUpParam;
+import mubex.renewal_foodsns.domain.dto.request.update.UpdateNickNameParam;
+import mubex.renewal_foodsns.domain.dto.request.update.UpdatePasswordParam;
 import mubex.renewal_foodsns.domain.dto.response.MemberResponse;
+import mubex.renewal_foodsns.domain.type.MemberRank;
 import mubex.renewal_foodsns.presentation.annotation.Api;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Api
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/member")
+@RequestMapping("/api/v1/members")
 @Validated
 public class MemberApi {
 
@@ -35,7 +38,8 @@ public class MemberApi {
         memberService.signUp(
                 signUpParam.email(),
                 signUpParam.nickName(),
-                signUpParam.password()
+                signUpParam.password(),
+                signUpParam.profileId()
         );
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
@@ -61,14 +65,14 @@ public class MemberApi {
     }
 
     @PatchMapping("/nick-name")
-    public ResponseEntity<String> updateNickName(@RequestBody @Valid UpdateNickNameParam updateNickNameParam) {
+    public ResponseEntity<MemberResponse> updateNickName(@RequestBody @Valid UpdateNickNameParam updateNickNameParam) {
 
-        String updateNickName = memberService.updateNickName(
+        MemberResponse memberResponse = memberService.updateNickName(
                 updateNickNameParam.nickName(),
                 updateNickNameParam.updatedNickName()
         );
 
-        return ResponseEntity.ok(updateNickName);
+        return ResponseEntity.ok(memberResponse);
     }
 
     @PatchMapping("/password")
@@ -79,6 +83,30 @@ public class MemberApi {
                 updatePasswordParam.password()
         );
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("")
+    public ResponseEntity<Void> deleteMember(@RequestParam("nickName") String nickName) {
+
+        memberService.markAsDeleted(nickName);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/rank")
+    public ResponseEntity<List<MemberResponse>> findMemberRank(@RequestParam("memberRank") MemberRank memberRank) {
+
+        List<MemberResponse> memberResponses = memberService.findByMemberRank(memberRank);
+
+        return ResponseEntity.ok(memberResponses);
+    }
+
+    @GetMapping("/page")
+    public ResponseEntity<Page<MemberResponse>> pagingByDescSort(PageRequest pageRequest) {
+
+        Page<MemberResponse> memberResponses = memberService.viewPagingMemberRank(pageRequest);
+
+        return ResponseEntity.ok(memberResponses);
     }
 }
