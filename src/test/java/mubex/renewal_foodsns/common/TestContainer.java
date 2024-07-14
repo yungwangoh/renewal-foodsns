a1f2ee76ce169c603bf73b313dd7ea52f095c260
swager253 <swager253@naver.com>
2024-07-14 14:51:09 +0900

    remove: TestInstance annotation 제거

--- src/test/java/mubex/renewal_foodsns/common/TestContainer.java
+++ src/test/java/mubex/renewal_foodsns/common/TestContainer.java
@@ -2,15 +2,12 @@ package mubex.renewal_foodsns.common;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@TestInstance(Lifecycle.PER_METHOD)
 @Testcontainers
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
