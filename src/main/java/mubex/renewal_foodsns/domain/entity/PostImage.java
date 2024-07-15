3c35f53f73fa74b28dabe3b6068a4d9c8c7ac324
swager253 <swager253@naver.com>
2024-07-15 13:38:33 +0900

    feat: origin file name 필드명 추가

--- src/main/java/mubex/renewal_foodsns/domain/entity/PostImage.java
+++ src/main/java/mubex/renewal_foodsns/domain/entity/PostImage.java
@@ -1,5 +1,6 @@
 package mubex.renewal_foodsns.domain.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -10,16 +11,20 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class PostImage {
+public class PostImage extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "origin_file_name")
+    private String originFileName;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(
@@ -27,4 +32,10 @@ public class PostImage {
             foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT)
     )
     private Post post;
+
+    @Builder
+    public PostImage(String originFileName, Post post) {
+        this.originFileName = originFileName;
+        this.post = post;
+    }
 }
