2e883061182effe500e0bebc7300c7dacf196326
swager253 <swager253@naver.com>
2024-07-15 13:25:15 +0900

    feat: 게시물 이미지 예외 추가

--- src/main/java/mubex/renewal_foodsns/common/exception/ExceptionResolver.java
+++ src/main/java/mubex/renewal_foodsns/common/exception/ExceptionResolver.java
@@ -13,9 +13,10 @@ public enum ExceptionResolver implements ErrorResponse {
     NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "NOT_FOUND_000", "유저를 찾을 수 없습니다."),
     NOT_FOUND_POST(HttpStatus.NOT_FOUND, "NOT_FOUND_001", "게시물을 찾을 수 없습니다."),
     NOT_FOUND_COMMENT(HttpStatus.NOT_FOUND, "NOT_FOUND_002", "댓글을 찾을 수 없습니다."),
-    NOT_FOUND_COMMENT_HEART(HttpStatus.NOT_FOUND, "NOT_FOUNT_003", "댓글 좋아요를 찾을 수 없습니다."),
-    NOT_FOUND_POST_HEART(HttpStatus.NOT_FOUND, "NOT_FOUNT_004", "게시물 좋아요를 찾을 수 없습니다."),
-    NOT_FOUND_BLACK_LIST(HttpStatus.NOT_FOUND, "NOT_FOUNT_005", "블랙 리스트를 찾을 수 없습니다."),
+    NOT_FOUND_COMMENT_HEART(HttpStatus.NOT_FOUND, "NOT_FOUND_003", "댓글 좋아요를 찾을 수 없습니다."),
+    NOT_FOUND_POST_HEART(HttpStatus.NOT_FOUND, "NOT_FOUND_004", "게시물 좋아요를 찾을 수 없습니다."),
+    NOT_FOUND_BLACK_LIST(HttpStatus.NOT_FOUND, "NOT_FOUND_005", "블랙 리스트를 찾을 수 없습니다."),
+    NOT_FOUND_POST_IMAGE(HttpStatus.NOT_FOUND, "NOT_FOUND_006", "게시물 이미지를 찾을 수 없습니다."),
 
     // un_authorized 401
     LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "UN_AUTHORIZED_000", "로그인에 실패하였습니다."),
