ae95c0ace87b4de2009244887ad2c0fffd882e00
swager253 <swager253@naver.com>
2024-07-15 13:27:18 +0900

    feat: 게시물 image repo 추가

--- src/main/java/mubex/renewal_foodsns/domain/repository/PostImageRepository.java
+++ src/main/java/mubex/renewal_foodsns/domain/repository/PostImageRepository.java
@@ -1,4 +1,11 @@
 package mubex.renewal_foodsns.domain.repository;
 
+import java.util.List;
+import mubex.renewal_foodsns.domain.entity.PostImage;
+
 public interface PostImageRepository {
+
+    PostImage save(PostImage postImage);
+    PostImage findById(Long id);
+    List<PostImage> findAllByPostId(Long postId);
 }
