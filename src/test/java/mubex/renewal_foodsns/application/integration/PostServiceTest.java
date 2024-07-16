9492ab8d3bf81b8f5a8302f625826b776a6c4e93
swager253 <swager253@naver.com>
2024-07-16 21:51:49 +0900

    test: 게시물 서비스 통합, 유닛 테스트

---
+++ src/test/java/mubex/renewal_foodsns/application/integration/PostServiceTest.java
@@ -0,0 +1,107 @@
+package mubex.renewal_foodsns.application.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import mubex.renewal_foodsns.application.MemberService;
+import mubex.renewal_foodsns.application.PostService;
+import mubex.renewal_foodsns.common.TestContainer;
+import mubex.renewal_foodsns.domain.dto.response.PostResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.web.multipart.MultipartFile;
+
+@SpringBootTest
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@Sql(
+        value = "/data.sql",
+        executionPhase = ExecutionPhase.AFTER_TEST_METHOD
+)
+public class PostServiceTest extends TestContainer {
+
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private MemberService memberService;
+
+    @BeforeEach
+    void setUp() {
+        String email = "qwer1234@gmail.com";
+        String password = "qwer1234";
+        String nickName = "안녕";
+
+        memberService.signUp(email, nickName, password, 1);
+    }
+
+    @Test
+    void 게시물_저장() throws IOException {
+        // given
+        String title = "야호";
+        String text = "야호맨";
+        String nickName = "안녕";
+
+        List<MultipartFile> multipartFiles = getMultipartFiles();
+
+        // when
+        PostResponse postResponse = postService.create(title, text, nickName, multipartFiles);
+
+        // then
+        assertThat(postResponse.title()).isEqualTo(title);
+        assertThat(postResponse.text()).isEqualTo(text);
+        assertThat(postResponse.postImageResponses()).hasSize(getMultipartFiles().size());
+    }
+
+    @Test
+    void 게시물_업데이트() throws IOException {
+        // given
+        String title = "호야";
+        String text = "홀ㄴ아ㅣㅗㅎ렁나ㅣㅗㅎ";
+        String nickName = "안녕";
+
+        PostResponse postResponse = createPost();
+        List<MultipartFile> multipartFiles = getMultipartFiles();
+
+        // when
+        PostResponse update = postService.update(postResponse.id(), title, text, nickName, multipartFiles);
+
+        // then
+        assertThat(update.title()).isEqualTo(title);
+        assertThat(update.text()).isEqualTo(text);
+        assertThat(update.postImageResponses()).hasSize(getMultipartFiles().size());
+    }
+
+    private List<MultipartFile> getMultipartFiles() throws IOException {
+        String name = "image";
+        String originalFileName = "test.jpeg";
+        String contentType = "image/png";
+        String fileUrl = "/Users/yungwang-o/Documents/FoodSNS.png";
+
+        List<MultipartFile> mockMultipartFiles = new ArrayList<>();
+        mockMultipartFiles.add(
+                new MockMultipartFile(name, originalFileName, contentType, new FileInputStream(fileUrl))
+        );
+
+        return mockMultipartFiles;
+    }
+
+    private PostResponse createPost() throws IOException {
+        String title = "야호";
+        String text = "야호맨";
+        String nickName = "안녕";
+
+        List<MultipartFile> multipartFiles = getMultipartFiles();
+
+        return postService.create(title, text, nickName, multipartFiles);
+    }
+}
---
+++ src/test/java/mubex/renewal_foodsns/application/unit/PostServiceTest.java
@@ -0,0 +1,134 @@
+package mubex.renewal_foodsns.application.unit;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.anyString;
+import static org.mockito.BDDMockito.given;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import mubex.renewal_foodsns.application.PostImageService;
+import mubex.renewal_foodsns.application.PostService;
+import mubex.renewal_foodsns.common.mapper.map.PostMapper;
+import mubex.renewal_foodsns.common.mapper.map.PostPageMapper;
+import mubex.renewal_foodsns.domain.entity.Post;
+import mubex.renewal_foodsns.domain.repository.MemberRepository;
+import mubex.renewal_foodsns.domain.repository.PostHeartRepository;
+import mubex.renewal_foodsns.domain.repository.PostReportRepository;
+import mubex.renewal_foodsns.domain.repository.PostRepository;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayNameGeneration(ReplaceUnderscores.class)
+public class PostServiceTest {
+
+    @InjectMocks
+    private PostService postService;
+
+    @Mock
+    private PostRepository postRepository;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Mock
+    private PostImageService postImageService;
+
+    @Mock
+    private PostHeartRepository postHeartRepository;
+
+    @Mock
+    private PostReportRepository postReportRepository;
+
+    @Mock
+    private PostMapper postMapper;
+
+    @Mock
+    private PostPageMapper postPageMapper;
+
+    @Test
+    void 유저가_게시물_이미지를_10개를_초과하면_예외_발생() {
+
+        String title = "하이";
+        String text = "하이용";
+        String nickName = "매그내릭";
+
+        assertThatThrownBy(() -> postService.create(title, text, nickName, getMultipartFiles(11)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 유저가_게시물을_중복_해서_작성하면_예외_발생() {
+
+        String title = "하이";
+        String text = "하이용";
+        String nickName = "매그내릭";
+
+        given(postRepository.existsByTitle(anyString())).willReturn(true);
+
+        assertThatThrownBy(() -> postService.create(title, text, nickName, List.of()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 유저가_게시물_좋아요를_중복해서_누르면_예외_발생() {
+
+        String nickName = "하이";
+        Long postId = 1L;
+
+        given(postHeartRepository.existsByMemberNickName(anyString())).willReturn(true);
+
+        assertThatThrownBy(() -> postService.increaseHeart(nickName, postId))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 유저가_게시물_신고를_중복해서_누르면_예외_발생() {
+
+        String nickName = "하이";
+        Long postId = 1L;
+
+        given(postReportRepository.existsByMemberNickName(anyString())).willReturn(true);
+
+        assertThatThrownBy(() -> postService.increaseReport(nickName, postId))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 이미_삭제한_게시물을_삭제하려면_예외_발생() {
+
+        Long postId = 1L;
+
+        Post post = Post.create("안녕", "ㅎㅇ", 0, 0, 0, true, null);
+
+        given(postRepository.findById(anyLong())).willReturn(post);
+
+        assertThatThrownBy(() -> postService.delete(postId))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private List<MultipartFile> getMultipartFiles(int idx) throws IOException {
+        List<MultipartFile> files = new ArrayList<>();
+
+        String fileUrl = "/Users/yungwang-o/Documents/FoodSNS.png";
+
+        MultipartFile multipartFile = new MockMultipartFile("image", "image.jpg",
+                "image/jpa", new FileInputStream(fileUrl));
+
+        for (int i = 0; i < idx; i++) {
+            files.add(multipartFile);
+        }
+
+        return files;
+    }
+}
