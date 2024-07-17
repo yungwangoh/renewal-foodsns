b04673a8cb7204c7dedfc28e9ade07a6d43fef85
swager253 <swager253@naver.com>
2024-07-17 14:13:49 +0900

    refactor: property 환경 변수 경로 변경

--- src/main/java/mubex/renewal_foodsns/common/property/CloudProperties.java
+++ src/main/java/mubex/renewal_foodsns/common/property/CloudProperties.java
@@ -2,8 +2,9 @@ package mubex.renewal_foodsns.common.property;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "cloud.gcp.storage")
+@ConfigurationProperties(prefix = "spring.cloud.gcp.storage")
 public record CloudProperties(String bucket, Credentials credentials) {
 
-    public record Credentials(String location) {}
+    public record Credentials(String location) {
+    }
 }
