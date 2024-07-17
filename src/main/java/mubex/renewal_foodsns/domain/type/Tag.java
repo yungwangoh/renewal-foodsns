04697fd62a78b06590ab223fcd74fd6a4ae52e17
swager253 <swager253@naver.com>
2024-07-17 14:11:08 +0900

    feat: Tag 추가

---
+++ src/main/java/mubex/renewal_foodsns/domain/type/Tag.java
@@ -0,0 +1,17 @@
+package mubex.renewal_foodsns.domain.type;
+
+import lombok.Getter;
+
+@Getter
+public enum Tag {
+    NOODLE(1), // 면
+    SOUP(2), // 국, 찌개
+    SNACK(3), // 분식
+    ETC(4); // 기타
+
+    private final int value;
+
+    Tag(int value) {
+        this.value = value;
+    }
+}
