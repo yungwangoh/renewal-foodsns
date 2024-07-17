e5a40a749d34387bb5212c2c8b7a7b8e974d6fd9
swager253 <swager253@naver.com>
2024-07-17 14:42:14 +0900

    feat: mapper toResponseWithImage 추가

--- src/main/java/mubex/renewal_foodsns/common/mapper/map/PostMapper.java
+++ src/main/java/mubex/renewal_foodsns/common/mapper/map/PostMapper.java
@@ -1,6 +1,8 @@
 package mubex.renewal_foodsns.common.mapper.map;
 
+import java.util.List;
 import mubex.renewal_foodsns.common.mapper.Mappable;
+import mubex.renewal_foodsns.domain.dto.response.PostImageResponse;
 import mubex.renewal_foodsns.domain.dto.response.PostResponse;
 import mubex.renewal_foodsns.domain.entity.Post;
 import org.mapstruct.Mapper;
@@ -8,9 +10,24 @@ import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants.ComponentModel;
 
 @Mapper(componentModel = ComponentModel.SPRING)
-public interface PostMapper extends Mappable<PostResponse, Post>{
+public interface PostMapper extends Mappable<PostResponse, Post> {
 
     @Override
     @Mapping(source = "member", target = "memberResponse")
+    @Mapping(target = "postImageResponses", ignore = true)
     PostResponse toResponse(Post post);
+
+    default PostResponse toResponseWithImages(Post post, List<PostImageResponse> postImageResponses) {
+        PostResponse response = toResponse(post);
+        return new PostResponse(
+                response.id(),
+                response.title(),
+                response.text(),
+                response.heart(),
+                response.report(),
+                response.views(),
+                response.memberResponse(),
+                postImageResponses
+        );
+    }
 }
