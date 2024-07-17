2a554db76008448bdc72a127503ba5ff93588886
swager253 <swager253@naver.com>
2024-07-17 15:24:58 +0900

    fix: strategy 전략 추가 안해서 db 에러 발생

--- src/main/java/mubex/renewal_foodsns/domain/entity/PostReport.java
+++ src/main/java/mubex/renewal_foodsns/domain/entity/PostReport.java
@@ -5,9 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,9 +18,11 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@Table(name = "post_report")
 public class PostReport {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
